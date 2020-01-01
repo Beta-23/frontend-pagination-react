8e8884e8a6613ef7c7fe7fa6d7c67908e35bfd72
Beta-23 <alfred.dominguez@gmail.com>
2020-01-01 14:56:19 -0800

    Add axios and set up api to fetch posts data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
+  const [ posts, setPosts ] = useState([]);
+  const [ loading, setLoading ] = useState(false);
+  const [ currentPage, setCurrentPage ] = useState(1);
+  const [ postsPerPage, setPostsPerPage ] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    }
+    fetchPosts();
+  }, []);
+
+  // Test json data is hitting
+  // console.log(posts);
+
   return (
     <div className='container'>
       <h1>Hello World!</h1>
