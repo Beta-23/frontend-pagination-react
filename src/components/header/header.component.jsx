6b5405178313937e743349dced5a6dac0f12865c
Beta-23 <alfred.dominguez@gmail.com>
2020-01-02 00:20:23 -0800

    Add logo and style to header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './header.styles.css';
-
+import { ReactComponent as Logo } from '../../assets/BlogM.svg';
 
 const Header = ({ currentUser, hidden }) => (
   <div className='header'>
+  <Logo className='logo' />
     <a className='logo-container' href='/'>
-      
+        
     </a>
     <div className='options'>
       <a className='option' href='/blog'>
